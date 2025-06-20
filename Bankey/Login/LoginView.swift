127bad9caaf7e056b5dd0b795267a70303f2f7d7
Edwin Cardenas <ecardenas@lwolf.com>
2025-06-20 14:26:04 -0500

    feat: add login view and customize folder structure

---
+++ Bankey/Login/LoginView.swift
@@ -0,0 +1,41 @@
+//
+//  LoginView.swift
+//  Bankey
+//
+//  Created by Edwin Cardenas on 6/20/25.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+
+    // MARK: - Initializers
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        style()
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Helpers
+
+extension LoginView {
+
+    private func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    private func layout() {
+
+    }
+}
--- Bankey/Login/LoginViewController.swift
+++ Bankey/Login/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Bankey
 //
 //  Created by Edwin Cardenas on 6/20/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +15,4 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemMint
     }
 
-
 }
-
