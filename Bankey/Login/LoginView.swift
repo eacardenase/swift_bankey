ab206d2026bbf2ffda13001f08fbce199f5c18ac
Edwin Cardenas <ecardenas@lwolf.com>
2025-06-20 16:07:55 -0500

    feat: add username text field to login view

--- Bankey/Login/LoginView.swift
+++ Bankey/Login/LoginView.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class LoginView: UIView {
 
+    lazy var usernameTextField: UITextField = {
+        let textField = UITextField()
+
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "username"
+        textField.keyboardType = .asciiCapable
+        textField.delegate = self
+
+        return textField
+    }()
+
     override var intrinsicContentSize: CGSize {
         return CGSize(width: 200, height: 200)
     }
@@ -37,6 +48,44 @@ extension LoginView {
     }
 
     private func layout() {
+        addSubview(usernameTextField)
+
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(
+                equalTo: topAnchor,
+                constant: 8
+            ),
+            usernameTextField.leadingAnchor.constraint(
+                equalTo: leadingAnchor,
+                constant: 8
+            ),
+            usernameTextField.trailingAnchor.constraint(
+                equalTo: trailingAnchor,
+                constant: -8
+            ),
+        ])
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension LoginView: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+
+        return true
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
 
     }
 }
